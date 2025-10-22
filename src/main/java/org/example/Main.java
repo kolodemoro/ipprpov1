5fad2797f0425f368aea2ae6fb1d115afcec1884
Aliaksandr <sasha@gmail.com>
2025-10-22 18:58:19 +0300

    добавлены заглушки для будущих методов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
-    
+    // Заглушка для варианта 9
+    public static boolean isPalindrome(String word) {
+        return false;
+    }
+
+    // Заглушка для варианта 6
+    public static int maxOfThree(int a, int b, int c) {
+        return 0;
+    }
+
+    // Заглушки для варианта 1
+    public static double add(double a, double b) {
+        return 0;
+    }
+    public static double subtract(double a, double b) {
+        return 0;
+    }
+    public static double multiply(double a, double b) {
+        return 0;
+    }
+    public static double divide(double a, double b) {
+        return 0;
+    }
 }
